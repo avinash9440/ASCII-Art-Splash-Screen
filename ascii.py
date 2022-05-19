d705ae41c4f6ce4613b5017ead86d5a7a8a39bcd
avinash9440 <54437855+avinash9440@users.noreply.github.com>
2022-05-19 18:14:00 +0530

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 import random
 import os
 i = random.randrange(9) + 1 
-website="https://raw.githubusercontent.com/avinash9440/ASCII-Art-Splash-Screen/tree/master/art"+ str(i) + ".txt"
+website="https://raw.githubusercontent.com/avinash9440/ASCII-Art-Splash-Screen/master/art"+ str(i) + ".txt"
 os.system("curl " +  website)
